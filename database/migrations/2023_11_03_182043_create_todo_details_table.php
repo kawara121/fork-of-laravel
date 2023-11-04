0770f276b28c9da76371bcd430c96bbafe18636b
kawara121 <syouta1216@gmail.com>
2023-11-04 20:34:50 +0900

    注文情報表示からToDo情報表示に仕様を変更

---
+++ database/migrations/2023_11_03_182043_create_todo_details_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('todo_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('todo_id');
+            $table->text('description');
+            $table->timestamps();
+
+            $table->foreign('todo_id')->references('id')->on('todos')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('todo_details');
+    }
+};
